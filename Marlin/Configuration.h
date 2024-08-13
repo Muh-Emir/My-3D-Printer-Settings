2d74cbb02037496659278d1cad453cbe5d2613bd
Emir Genç <68015444+Muh-Emir@users.noreply.github.com>
2024-08-14 02:14:28 +0300

    14/08/2024 Ayar denemesi

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -608,7 +608,7 @@
  *                                      X, Y, Z, E0 [, E1[, E2[, E3[, E4]]]]
  */
  //senkronizasyon
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 83, 82, 1590, 101 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 83, 82, 1590, 91 }
 
 /**
  * Default Max Feed Rate (mm/s)
@@ -1221,7 +1221,7 @@
 // M501 - reads parameters from EEPROM (if you need reset them after you changed them temporarily).
 // M502 - reverts to the default "factory settings".  You still need to store them in EEPROM afterwards if you want to.
 //
-//#define EEPROM_SETTINGS // Enable for M500 and M501 commands
+#define EEPROM_SETTINGS // Enable for M500 and M501 commands
 //#define DISABLE_M503    // Saves ~2700 bytes of PROGMEM. Disable for release!
 #define EEPROM_CHITCHAT   // Give feedback on EEPROM commands. Disable to save PROGMEM.
 
